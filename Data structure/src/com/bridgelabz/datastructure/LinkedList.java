771ee1a2077619070204930cf8d5e10aa87788f5
pranaybhujade <pranaybhujade@gmail.com>
2022-09-26 20:14:05 +0530

    simple linkedlist

--- Data structure/src/com/bridgelabz/datastructure/LinkedList.java
+++ Data structure/src/com/bridgelabz/datastructure/LinkedList.java
@@ -2,6 +2,14 @@ package com.bridgelabz.datastructure;
 
 public class LinkedList {
     public static void main(String[] args) {
-        System.out.println("welcome");
+        Node<Integer> node3=new Node<>(70);
+        node1.setNext(node2);
+        node2.setNext(node3);
+
     }
-}
+    }
+public class Node<K> {
+
+    private K key;
+    private Node next;
+
