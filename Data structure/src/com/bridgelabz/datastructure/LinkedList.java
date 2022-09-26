96f2970812dd8e21ae35191a9678f362f9ef184a
pranaybhujade <pranaybhujade@gmail.com>
2022-09-26 20:38:15 +0530

    adding nodes

--- Data structure/src/com/bridgelabz/datastructure/LinkedList.java
+++ Data structure/src/com/bridgelabz/datastructure/LinkedList.java
@@ -1,15 +1,34 @@
 package com.bridgelabz.datastructure;
 
 public class LinkedList {
-    public static void main(String[] args) {
-        Node<Integer> node3=new Node<>(70);
-        node1.setNext(node2);
-        node2.setNext(node3);
+    public class LinkedList<K> {
+        private Node head;
+        private Node tail;
 
-    }
-    }
-public class Node<K> {
+        public void add(K key) {
+            Node<K> node = new Node<>(key);
+            if (head == null) {
+                this.head = node;
+                this.tail = node;
+            } else {
+                node.setNext(head);
+                head = node;
+            }
+        }
 
-    private K key;
-    private Node next;
+        public static void main(String[] args) {
+            Node<Integer> node1 = new Node<>(56);
+            Node<Integer> node2 = new Node<>(30);
+            Node<Integer> node3 = new Node<>(70);
+            node1.setNext(node2);
+            node2.setNext(node3);
+
+            LinkedList linkedList = new LinkedList();
+            linkedList.add(56);
+            linkedList.add(30);
+            linkedList.add(70);
+        }
+    }
+}
+
 
